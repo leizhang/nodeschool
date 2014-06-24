c5e4bae8b9ab67c82adee4651ffa975ba0ee7be1
Lei Zhang <stonenot@gmail.com>
2014-06-23 18:53:17 -0700

    remove comments

--- learnyounode/04-my-first-async-io.js
+++ learnyounode/04-my-first-async-io.js
@@ -3,5 +3,3 @@ var paragraph = fs.readFile(process.argv[2],'utf8', function(err, data) {
  	if(err) throw err;
 	console.log(data.split("\n").length-1);
 })
-//console.log(paragraph)
-//console.log(paragraph.split("\n").length-1)
